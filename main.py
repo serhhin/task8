87d0d9a2e5d2ddc411999afdba8ac863057f38dc
serhhin <neprokaren41@gmail.com>
2024-02-04 13:18:28 +0200

    flake8 ...

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 from datetime import date, datetime, timedelta
 
+
 def get_birthdays_per_week(users):
+    
     today = date.today()
+    
     if not users:
         return {}  # Повертаємо пустий словник для порожнього списку користувачів
+    
     def find_start_of_next_week():
         days_until_monday = (7 - today.weekday()) % 7
         start_of_next_week = today + timedelta(days=days_until_monday)
-        return start_of_next_week
+        return start_of_next_week 
+     
     start_of_next_week = find_start_of_next_week()
-    def find_start_of_next_next_week():
-        days_until_monday = (7 - today.weekday()) % 7
-        find_start_of_next_next_week = today + timedelta(days=days_until_monday +5 )
-        return find_start_of_next_next_week
-    start_of_next_next_week = find_start_of_next_next_week()#2024-02-03
+      
     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    
     birthdays_per_week = {day: [] for day in days_of_week} #робить список
+    
     for user in users:
         birthday = user['birthday'].replace(year=today.year) # 2024-01-26
+        
         if birthday < today:
             birthday = birthday.replace(year=today.year + 1)   # Якщо день народження вже минув у цьому році, пропускаємо.
-        if birthday >= today + timedelta(days=7):
-            print(today + timedelta(days=7))
-            # Якщо день народження не на наступній неділі , пропускаємо користувача.
+            
+        if birthday >= today + timedelta(days=7):# Якщо день народження не на наступній неділі , пропускаємо користувача.   
             continue
+        
         if birthday.weekday() >= 5:
             while birthday.weekday() != start_of_next_week.weekday():
                 birthday += timedelta(days=1) # Зміщуємо день народження на наступний тиждень
-        #if birthday < start_of_next_next_week :
+                
         day_name = days_of_week[(birthday.weekday() - start_of_next_week.weekday()) % 7]
         birthdays_per_week[day_name].append(user['name'])
+        
     if not any(birthdays_per_week.values()):
         print({})
         return {}
+    
+    
     # Видаляємо дні тижня, для яких немає днів народжень
     birthdays_per_week = {day: names for day, names in birthdays_per_week.items() if names}
     return birthdays_per_week
+
 if __name__ == "__main__":
     users = [
         {"name": "Jan Koum1", "birthday": datetime(2015, 1,25).date()},
